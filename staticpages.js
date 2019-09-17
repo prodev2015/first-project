80d2e8c707497be5708ee20e4a472cadbbd85383
jimmybutler-2015 <image345678@outlook.com>
2019-09-18 03:28:09 +0800

    minor changes

--- staticpages.js
+++ staticpages.js
@@ -7,6 +7,37 @@ var mimeTypes = {
     '.html': 'text/html',
     '.css' : 'text/css'
 };
+
+var cache = {};
+function cacheAndDeliver(f, cb) {
+    fs.stat(f, function(err, stats) {
+        if(err) {
+            return console.log('Oh, no!, Error', err); 
+        }
+        var lastChanged = Date.parse(stats.ctime),
+        isUpdated = (cache[f]) && lastChanged > cache[f].timestamp;
+        if(!cache[f] || isUpdated) {
+            fs.readFile(f, function(err, data) {
+                console.log('loading ' + f + ' from file');
+                if (!err) {
+                    cache[f] = {content: data, timestamp: Date.now()} ;
+                }
+                    cb(err, data);
+            });
+        }
+    });
+    // if (!cache[f]) {
+    //     fs.readFile(f, function(err, data) {
+    //         if (!err) {
+    //             cache[f] = {content: data, timestamp: Date.now()} ;
+    //         }
+    //             cb(err, data);
+    //     }); 
+    //     return;
+    // }
+    console.log('loading ' + f + ' from cache');
+    cb(null, cache[f].content);
+}
 http.createServer(function (request, response) {
     var lookup = path.basename(decodeURI(request.url)) || 'index.html';
     console.log(lookup);
@@ -14,7 +45,7 @@ http.createServer(function (request, response) {
     fs.exists(f, function(exists) {
         console.log(exists ? lookup + " is there" :lookup + " doesn't exist");
         if(exists) {
-            fs.readFile(f, function(err, data) {
+            cacheAndDeliver(f, function(err, data) {
                 if(err) {
                     response.writeHead(500);
                     response.end('Server Error!');
@@ -22,8 +53,8 @@ http.createServer(function (request, response) {
                 }
                 var headers = {'Content-type': mimeTypes[path.extname(lookup)]};
                 response.writeHead(200, headers);
-                response.end(data);
-            });
+                response.end(data);    
+            })
             return;
         }
         response.writeHead(404);
